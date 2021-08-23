0555f8350635cacdee754540e70633f6bf82ea01
Kieran McClung <kieranmcclung@hotmail.com>
2021-08-23 13:54:26 +0100

    Init

---
+++ webpack.mix.js
@@ -0,0 +1,13 @@
+let mix = require('laravel-mix');
+
+mix.js('src/js/script.js', 'dist/js');
+
+mix.sass('src/scss/style.scss', 'dist/css', {
+	sassOptions: {
+		outputStyle: 'expanded'
+	}
+}).options({
+	postCss: [
+		require('postcss-custom-properties')
+	]
+});
